4348f22ca06f973d274b095da3147caf713e8660
Maykon dos Santos <maykon.dos.santos@avanade.com>
2019-01-09 16:43:15 -0200

    first commit

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  View
+} from 'react-native';
+
+import Torch from 'react-native-torch';
+
+export default class App extends Component {
+  state = {
+    isAlight: false
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => {
+            this.setState({
+              isAlight: !this.state.isAlight
+            })
+            Torch.switchState(this.state.isAlight)
+          }}
+        >
+          <Image
+            style={styles.btnImage}
+            source={require('./assets/images/power.png')}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000',
+  },
+  btnImage: {
+    width: 100,
+    height: 100,
+  },
+});
